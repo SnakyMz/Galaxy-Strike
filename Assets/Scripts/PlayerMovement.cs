59fcf7e8262248fed4477bb9458f4278e279863c
Soban Syeed <Soban.syeed@outlook.com>
2025-04-07 19:34:55 +0530

    Add Movement restrictions to player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float controlSpeed = 50f;
+    [SerializeField] float clampXRange = 20f;
+    [SerializeField] float clampYRange = 15f;
 
     Vector2 movement;
 
@@ -15,9 +17,14 @@ public class PlayerMovement : MonoBehaviour
     private void ProcessTranslation()
     {
         float xOffset = movement.x * controlSpeed * Time.deltaTime;
-        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -clampXRange, clampXRange);
 
-        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
+        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -clampYRange, clampYRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
     }
 
     public void OnMove(InputValue value)
