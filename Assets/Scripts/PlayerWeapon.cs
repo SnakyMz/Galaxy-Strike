0e20095c94743bb4476f4c54492adef77f2bca7b
Soban Syeed <Soban.syeed@outlook.com>
2025-04-11 21:00:41 +0530

    Testing OnFire method

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -1,16 +1,17 @@
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class PlayerWeapon : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnFire(InputValue value)
+    {
+        Debug.Log("Fire");
     }
 }
