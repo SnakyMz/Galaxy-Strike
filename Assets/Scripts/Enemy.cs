5e0dd261db4240e4fe694ab2185141d53f30d6bb
Soban Syeed <Soban.syeed@outlook.com>
2025-04-28 07:56:44 +0530

    Add hitpoints functionality to Enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destroyedVFX;
-
+    [SerializeField] int hitpoints = 4;
     void OnParticleCollision(GameObject other)
     {
-        Instantiate(destroyedVFX, transform.position, Quaternion.identity);
-        Destroy(gameObject);
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        hitpoints--;
+
+        if (hitpoints <= 0)
+        {
+            Instantiate(destroyedVFX, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
     }
 }
