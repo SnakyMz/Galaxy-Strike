5e95245b3f9d1af54e01a1da56a1f326fb7d01ef
Soban Syeed <Soban.syeed@outlook.com>
2025-04-28 08:22:01 +0530

    Add Scoreboard functionality

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,14 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destroyedVFX;
     [SerializeField] int hitpoints = 4;
+    [SerializeField] int scoreValue = 10;
+
+    Scoreboard scoreboard;
+
+    void Start()
+    {
+        scoreboard = FindFirstObjectByType<Scoreboard>();
+    }
     void OnParticleCollision(GameObject other)
     {
         ProcessHit();
@@ -15,6 +23,7 @@ public class Enemy : MonoBehaviour
 
         if (hitpoints <= 0)
         {
+            scoreboard.IncreaseScore(scoreValue);
             Instantiate(destroyedVFX, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
